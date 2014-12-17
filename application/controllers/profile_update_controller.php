37931305b2a5397b2191a5aa5ce777318f1bf044
alext_000 <alext_000@VAIO>
2014-12-17 19:05:49 +0200

    update_profile updates

--- application/controllers/profile_update_controller.php
+++ application/controllers/profile_update_controller.php
@@ -5,20 +5,8 @@
     include '../models/company.php';
     mysql_query("SET NAMES utf8");
     
-    $comp_name = filter_input(INPUT_POST, 'comp_name');
-    $display_name = filter_input(INPUT_POST, 'display_name');
-    $password = filter_input(INPUT_POST, 'password');
-    $email = filter_input(INPUT_POST, 'email');
-    $phone = filter_input(INPUT_POST, 'phone');
-    $city = filter_input(INPUT_POST, 'city');
-    $address = filter_input(INPUT_POST, 'address');  
-    $postal_code = filter_input(INPUT_POST, 'postal_code');
-    $latitude = filter_input(INPUT_POST, 'latitude');
-    $longitude= filter_input(INPUT_POST, 'longitude');    
-    
     $result = AutofillUpdateProfileForm($_SESSION['id']);
-    $_SESSION['upd_id'] = $result['id'];
-    $_SESSION['upd_comp_name'] = 'aaaaaaaaaaa'; 
+    $_SESSION['upd_comp_name'] = $result['comp_name'];
     $_SESSION['upd_display_name'] = $result['display_name']; 
     $_SESSION['upd_password'] = $result['password'];  
     $_SESSION['upd_email'] = $result['email'];  
@@ -29,69 +17,88 @@
     $_SESSION['upd_latitude'] = $result['latitude'];  
     $_SESSION['upd_longitude'] = $result['longitude']; 
     
-    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_]*$/",$comp_name)) {
+    $_SESSION['upd_comp_name'] = filter_input(INPUT_POST, 'comp_name');
+    $_SESSION['upd_display_name'] = filter_input(INPUT_POST, 'display_name');
+    $_SESSION['upd_password'] = filter_input(INPUT_POST, 'password');
+    $_SESSION['upd_email'] = filter_input(INPUT_POST, 'email');
+    $_SESSION['upd_phone'] = filter_input(INPUT_POST, 'phone');
+    $_SESSION['upd_city'] = filter_input(INPUT_POST, 'city');
+    $_SESSION['upd_address'] = filter_input(INPUT_POST, 'address');  
+    $_SESSION['upd_postal_code'] = filter_input(INPUT_POST, 'postal_code');
+    $_SESSION['upd_latitude'] = filter_input(INPUT_POST, 'latitude');
+    $_SESSION['upd_longitude'] = filter_input(INPUT_POST, 'longitude');    
+    
+    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_][a-zA-Z0-9\x80-\xFF_]*$/",$_SESSION['upd_comp_name'])) {
          $_SESSION['error_comp_name'] = "8 έως 20 χαρακτήρες ή αριθμούς και underscore"; 
+         $_SESSION['update_profile_error'] = true;
     }    
-    else if (empty ($comp_name)){
+    else if (empty ($_SESSION['upd_comp_name'])){
         $_SESSION['error_comp_name'] = "Απαιτείται όνομα χρήστη";
         $_SESSION['update_profile_error'] = true;
     }
     
-    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_]*$/",$password)) {
+    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_][a-zA-Z0-9\x80-\xFF\_]*$/",$_SESSION['upd_password'])) {
          $_SESSION['error_password'] = "8 έως 20 χαρακτήρες ή αριθμούς και underscore"; 
+         $_SESSION['update_profile_error'] = true;
     }   
-    else if (empty ($password)){
+    else if (empty ($_SESSION['upd_password'])){
         $_SESSION['error_password'] = "Απαιτείται κωδικός";
         $_SESSION['update_profile_error'] = true;
     }
     
-   if (!preg_match("/^[a-zA-Z0-9\x80-\xFF ]*$/",$display_name)) {
+   if (!preg_match("/^[a-zA-Z0-9\x80-\xFF ]*$/",$_SESSION['upd_display_name'])) {
          $_SESSION['error_display_name'] = "Μόνο χαρακτήρες, αριθμοί και κενά επιτρέπονται"; 
+         $_SESSION['update_profile_error'] = true;
    }    
-   else if (empty ($display_name)){
+   else if (empty ($_SESSION['upd_display_name'])){
         $_SESSION['error_display_name'] = "Απαιτείται επωνυμία καταστήματος";
         $_SESSION['update_profile_error'] = true;
     }
 
-   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+   if (!filter_var($_SESSION['upd_email'], FILTER_VALIDATE_EMAIL)) {
          $_SESSION['error_email'] = "Λανθασμένη μορφή email"; 
+         $_SESSION['update_profile_error'] = true;
    }
-   else if (empty ($email)){
+   else if (empty ($_SESSION['upd_email'])){
         $_SESSION['error_email'] = "Απαιτείται email";
         $_SESSION['update_profile_error'] = true;
    }
    
-   if (!preg_match("/^[0-9]{10}$/",$phone)) {
+   if (!preg_match("/^[0-9]{10}$/",$_SESSION['upd_phone'])) {
          $_SESSION['error_phone'] = "Απαιτούνται ακριβώς 10 νούμερα"; 
+         $_SESSION['update_profile_error'] = true;
    }    
-   else if (empty ($phone)){
+   else if (empty ($_SESSION['upd_phone'])){
         $_SESSION['error_phone'] = "Απαιτείται τηλέφωνο";
         $_SESSION['update_profile_error'] = true;
     }  
     
-    if (empty ($city)){
+    if (empty ($_SESSION['upd_city'])){
         $_SESSION['error_city'] = "Απαιτείται πόλη";
         $_SESSION['update_profile_error'] = true;
     }
     
-    if (empty ($address)){
+    if (empty ($_SESSION['upd_address'])){
         $_SESSION['error_address'] = "Απαιτείται διεύθυνση";
         $_SESSION['update_profile_error'] = true;
     }
     
-    if (empty ($latitude)){
+    if (empty ($_SESSION['upd_latitude'])){
         $_SESSION['error_latitude'] = "Απαιτείται γεωγραφικό πλάτος";
         $_SESSION['update_profile_error'] = true;
     }
     
-    if (empty ($longitude)){
+    if (empty ($_SESSION['error_longitude'])){
         $_SESSION['error_longitude'] = "Απαιτείται γεωγραφικό μήκος";
         $_SESSION['update_profile_error'] = true;
     }
     
     if (!isset($_SESSION['update_profile_error'])) {
-        $update_profile = UpdateProfile($comp_name, $display_name, $password, $email, $phone, $city, $address, $postal_code, $latitude, $longitude, $_SESSION['id']);
+        $update_profile = UpdateProfile($_SESSION['upd_comp_name'] , $_SESSION['upd_display_name'] , 
+            $_SESSION['upd_password'] , $_SESSION['upd_email'] , $_SESSION['upd_phone'] , $_SESSION['upd_city'] , 
+            $_SESSION['upd_address'] , $_SESSION['upd_postal_code'] , $_SESSION['upd_latitude'] , 
+            $_SESSION['upd_longitude'], $_SESSION['id']);
         $_SESSION['success'] = "Επιτυχής επεξεργασία!";
     }
-    unset ($_SESSION['register_error']); 
+    unset ($_SESSION['update_profile_error'] ); 
     header( 'Location: ../views/profile_update_view.php');
--- application/views/profile_update.php
+++ application/views/profile_update.php
@@ -6,7 +6,13 @@
 ?>
 
 <form action="../controllers/profile_update_controller.php" method="post" class="forms">
-    <label>Όνομα χρήστη:</label><input type='text' name='comp_name' maxlength='20' id='reg_username' value="<?php echo $_SESSION['upd_comp_name']; ?>">
+    <label>Όνομα χρήστη:</label><input type='text' name='comp_name' maxlength='20' id='reg_username'
+    <?php
+        if (isset($_SESSION['upd_comp_name'])){
+            echo "value=" . $_SESSION['upd_comp_name'] ;
+        }
+        unset ($_SESSION['upd_comp_name']);
+    ?> /> 
     <span class='error'>
     <?php if (isset ($_SESSION['error_comp_name'])) {
         echo $_SESSION['error_comp_name']; 
@@ -15,7 +21,13 @@
     ?></span>
     <br />
     <br />
-    <label>Επωνυμία καταστήματος:</label><input type='text' name='display_name' maxlength='20' id='reg_display' value="<?php echo $_SESSION['upd_display_name']; ?>">
+    <label>Επωνυμία καταστήματος:</label><input type='text' name='display_name' maxlength='20' id='reg_display'
+    <?php
+        if (isset($_SESSION['upd_display_name'])){
+            echo "value=" . $_SESSION['upd_display_name'] ;
+        }
+        unset ($_SESSION['upd_display_name']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_display_name'])) {
         echo $_SESSION['error_display_name']; 
@@ -24,7 +36,13 @@
     ?></span>
     <br />
     <br />
-    <label>Κωδικός:</label><input type='password' name='password' maxlength='20' id='reg_password' value="<?php echo $_SESSION['upd_password']; ?>">
+    <label>Κωδικός:</label><input type='password' name='password' maxlength='20' id='reg_password'
+    <?php
+        if (isset($_SESSION['upd_password'])){
+            echo "value=" . $_SESSION['upd_password'] ;
+        }
+        unset ($_SESSION['upd_password']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_password'])) {
         echo $_SESSION['error_password']; 
@@ -33,7 +51,13 @@
     ?></span>
     <br />
     <br />
-    <label>E-mail:</label><input type='text' name='email' maxlength='25' id='reg_email' value="<?php echo $_SESSION['upd_email']; ?>">
+    <label>E-mail:</label><input type='text' name='email' maxlength='25' id='reg_email'
+    <?php
+        if (isset($_SESSION['upd_email'])){
+            echo "value=" . $_SESSION['upd_email'] ;
+        }
+        unset ($_SESSION['upd_email']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_email'])) {
         echo $_SESSION['error_email']; 
@@ -42,7 +66,13 @@
     ?></span>
     <br />
     <br />
-    <label>Τηλέφωνο:</label><input type='text' name='phone' maxlength='10' id='reg_phone' value="<?php echo $_SESSION['upd_phone']; ?>">
+    <label>Τηλέφωνο:</label><input type='text' name='phone' maxlength='10' id='reg_phone'
+    <?php
+        if (isset($_SESSION['upd_phone'])){
+            echo "value=" . $_SESSION['upd_phone'] ;
+        }
+        unset ($_SESSION['upd_phone']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_phone'])) {
         echo $_SESSION['error_phone']; 
@@ -56,7 +86,13 @@
     για να επιλέξετε την ακριβή τοποθεσία του καταστήματος σας στο χάρτη.
     <br />
     <br /> 
-    <label>Πόλη:</label><input type='text' name='city' maxlength='25' readonly id='reg_city' value="<?php echo $_SESSION['upd_city']; ?>">
+    <label>Πόλη:</label><input type='text' name='city' maxlength='25' readonly id='reg_city'
+    <?php
+        if (isset($_SESSION['upd_city'])){
+            echo "value=" . $_SESSION['upd_city'] ;
+        }
+        unset ($_SESSION['upd_city']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_city'])) {
         echo $_SESSION['error_city']; 
@@ -65,7 +101,13 @@
     ?></span>
     <br />
     <br />
-    <label>Διεύθυνση:</label><input type='text' name='address' maxlength='50' readonly id='reg_address' value="<?php echo $_SESSION['upd_address']; ?>">
+    <label>Διεύθυνση:</label><input type='text' name='address' maxlength='50' readonly id='reg_address'
+    <?php
+        if (isset($_SESSION['upd_address'])){
+            echo "value=" . $_SESSION['upd_address'] ;
+        }
+        unset ($_SESSION['upd_address']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_address'])) {
         echo $_SESSION['error_address']; 
@@ -74,7 +116,13 @@
     ?></span>
     <br />
     <br />
-    <label>Ταχυδρομικός κώδικας:</label><input type='text' name='postal_code' maxlength='6' readonly id='reg_postal_code' value="<?php echo $_SESSION['upd_postal_code']; ?>">
+    <label>Ταχυδρομικός κώδικας:</label><input type='text' name='postal_code' maxlength='6' readonly id='reg_postal_code'
+    <?php
+        if (isset($_SESSION['upd_postal_code'])){
+            echo "value=" . $_SESSION['upd_postal_code'] ;
+        }
+        unset ($_SESSION['upd_postal_code']);
+    ?> />
     <span class='error'>
     <?php if (isset ($_SESSION['error_postal_code'])) {
         echo $_SESSION['error_postal_code']; 
@@ -83,7 +131,13 @@
     ?></span>
     <br />
     <br />
-    <label>Γεωγραφικό πλάτος:</label><input type='text' name='latitude' id='info_lat' readonly value="<?php echo $_SESSION['upd_latitude']; ?>">
+    <label>Γεωγραφικό πλάτος:</label><input type='text' name='latitude' id='info_lat' readonly
+    <?php
+        if (isset($_SESSION['upd_latitude'])){
+            echo "value=" . $_SESSION['upd_latitude'] ;
+        }
+        unset ($_SESSION['upd_latitude']);
+    ?> /> 
     <span class='error'>
     <?php if (isset ($_SESSION['error_latitude'])) {
         echo $_SESSION['error_latitude']; 
@@ -92,7 +146,13 @@
     ?></span>
     <br />
     <br />
-    <label>Γεωγραφικό μήκος:</label><input type='text' name='longitude' id='info_lng' readonly value="<?php echo $_SESSION['upd_longitude']; ?>">
+    <label>Γεωγραφικό μήκος:</label><input type='text' name='longitude' id='info_lng' readonly
+    <?php
+        if (isset($_SESSION['upd_longitude'])){
+            echo "value=" . $_SESSION['upd_longitude'] ;
+        }
+        unset ($_SESSION['upd_longitude']);
+    ?> /> 
     <span class='error'>
     <?php if (isset ($_SESSION['error_longitude'])) {
         echo $_SESSION['error_longitude']; 
@@ -109,4 +169,3 @@
         unset ($_SESSION['success']);
     }
     include 'footer.php';
-?>
