ddcdba66f6eb3b725e5c814843d3e44dc55f6a3f
Giannis <Giannis@mypc>
2014-12-17 15:15:02 +0200

    Improved add_offer service

--- application/controllers/add_offer_controller.php
+++ application/controllers/add_offer_controller.php
@@ -5,62 +5,64 @@
     include '../models/category.php';
     include '../models/offer.php';
     
-    $offer_name = filter_input(INPUT_POST, 'offer_name');
-    $offer_descr = filter_input(INPUT_POST, 'offer_descr');
-    $cat_id = filter_input(INPUT_POST, 'cat_id');
-    $start_date = filter_input(INPUT_POST, 'start_date');
-    $end_date = filter_input(INPUT_POST, 'end_date');
-    $discount = filter_input(INPUT_POST, 'discount');
-    $price = filter_input(INPUT_POST, 'price');
+    $_SESSION['offer_name'] = filter_input(INPUT_POST, 'offer_name');
+    $_SESSION['offer_descr'] = filter_input(INPUT_POST, 'offer_descr');
+    $_SESSION['cat_id'] = filter_input(INPUT_POST, 'cat_id');
+    $_SESSION['start_date'] = filter_input(INPUT_POST, 'start_date');
+    $_SESSION['end_date'] = filter_input(INPUT_POST, 'end_date');
+    $_SESSION['discount'] = filter_input(INPUT_POST, 'discount');
+    $_SESSION['price'] = filter_input(INPUT_POST, 'price');
     
-    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF\!\%\&\-\+ ]*$/",$offer_name)) {
+    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF\!\%\&\-\+ ]*/$",$_SESSION['offer_name'])) {
         $_SESSION['error_name'] = "Μόνο χαρακτήρες, αριθμοί και κενά επιτρέπονται"; 
         $_SESSION['offer_error'] = true;
     }
-    else if (empty ($offer_name)){
+    else if (empty($_SESSION['offer_name'])){
         $_SESSION['error_name'] = "Απαιτείται όνομα προσφοράς";
         $_SESSION['offer_error'] = true;
     }
     
-    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF\.\!\%\&\-\+ ]*$/",$offer_descr)) {
+    if (!preg_match("/^[a-zA-Z0-9\x80-\xFF\.\!\%\&\-\+ ]*$/",$_SESSION['offer_descr'])) {
         $_SESSION['error_descr'] = "Μόνο χαρακτήρες, αριθμοί και κενά επιτρέπονται"; 
         $_SESSION['offer_error'] = true;
     }    
     
-    if (empty ($start_date)){
+    if (empty ($_SESSION['start_date'])){
         $_SESSION['error_sdate'] = "Απαιτείται ημερομηνία έναρξης";
         $_SESSION['offer_error'] = true;
     }
     
-    if (empty ($end_date)){
+    if (empty ($_SESSION['end_date'])){
         $_SESSION['error_edate'] = "Απαιτείται ημερομηνία έναρξης";
         $_SESSION['offer_error'] = true;
     }
     
-    if (!preg_match("/^[0-9.]*$/",$discount)) {
+    if (!preg_match("/^[0-9.]*$/",$_SESSION['discount'])) {
         $_SESSION['error_discount'] = "Μόνο αριθμοί και η διαχωριστική . επιτρέπονται"; 
         $_SESSION['offer_error'] = true;
     }
-    else if ($discount > 100){
-        $_SESSION['error_discount'] = "Δεν μπορείτε να έχετε έκπτωση πάνω απο 100%";
+    else if ($_SESSION['discount'] > 100 || $_SESSION['discount'] < 0){
+        $_SESSION['error_discount'] = "Εισάγετε μια έγκυρη τιμή";
         $_SESSION['offer_error'] = true;
     }
-    else if (empty ($discount)){
+    else if (empty ($_SESSION['discount'])){
         $_SESSION['error_discount'] = "Aπαιτείται ποσοστό προσφοράς";
         $_SESSION['offer_error'] = true;
     }
     
-    if (!preg_match("/^[0-9.]*$/",$price)) {
+    if (!preg_match("/^[0-9.]*$/",$_SESSION['price'])) {
         $_SESSION['error_price'] = "Μόνο αριθμοί και η διαχωριστική . επιτρέπονται"; 
         $_SESSION['offer_error'] = true;
     }
-    else if (empty ($price)){
+    else if (empty ($_SESSION['price'])){
         $_SESSION['error_price'] = "Απαιτείται τιμή";
         $_SESSION['offer_error'] = true;
     }
         
     if (!isset($_SESSION['offer_error'])) {
-        $new_offer = AddOffer($offer_name, $offer_descr, $cat_id, $start_date, $end_date, $discount, $price, $_SESSION['id']);
+        $new_offer = AddOffer($_SESSION['offer_name'], $_SESSION['offer_descr'], 
+                $_SESSION['cat_id'], $_SESSION['start_date'], $_SESSION['end_date'], 
+                $_SESSION['discount'], $_SESSION['price'], $_SESSION['id']);
         $_SESSION['success'] = "Η προσφορά καταχωρήθηκε επιτυχώς!";
     }
     unset ($_SESSION['offer_error']);
--- application/views/add_offer.php
+++ application/views/add_offer.php
@@ -5,7 +5,13 @@
     include 'menu.php';
 ?>
 <form action="../controllers/add_offer_controller.php" method="post" class="forms">
-	<label>Όνομα προσφοράς:</label><input type='text' name='offer_name' maxlength='30' id='add_offer_name'></input>
+	<label>Όνομα προσφοράς:</label><input type='text' name='offer_name' maxlength='30' id='add_offer_name' 
+        <?php
+        if (isset($_SESSION['offer_name'])){
+            echo "value=" . $_SESSION['offer_name'] ;
+        }
+        unset ($_SESSION['offer_name']);
+        ?> >
         <span class='error'>* 
         <?php if (isset ($_SESSION['error_name'])) {
                 echo $_SESSION['error_name']; 
@@ -14,7 +20,12 @@
         ?></span>
         <br />
         <br />
-        <label>Περιγραφή προσφοράς:</label><textarea name='offer_descr' maxlength='160' id="add_offer_descr" rows='5' cols='57'></textarea>
+        <label>Περιγραφή προσφοράς:</label><textarea name='offer_descr' maxlength='160' id="add_offer_descr" rows='5' cols='57'><?php
+            if (isset($_SESSION['offer_descr'])){
+                echo $_SESSION['offer_descr'] ;
+            }
+            unset ($_SESSION['offer_descr']);
+        ?></textarea>
         <span class='error'>
         <?php if (isset ($_SESSION['error_descr'])) {
                 echo $_SESSION['error_descr']; 
@@ -27,7 +38,13 @@
         <span class='error'>*</span>
         <br />
         <br />
-        <label>Ημερομηνία έναρξης προσφοράς:</label><input type='text' id='demo1' maxlength='10' name='start_date' readonly size='10'></input>
+        <label>Ημερομηνία έναρξης προσφοράς:</label><input type='text' id='demo1' maxlength='10' name='start_date' readonly size='10'
+        <?php
+            if (isset($_SESSION['start_date'])){
+                echo "value=" . $_SESSION['start_date'] ;
+            }
+            unset ($_SESSION['start_date']);
+        ?> >
         <img src="../../public/js/date_picker/images2/cal.gif" onclick="javascript:NewCssCal('demo1','yyyyMMdd','','','','','future')" style="cursor:pointer"/> 
         <span class='error'>* 
         <?php if (isset ($_SESSION['error_sdate'])) {
@@ -37,7 +54,13 @@
         ?></span> 
         <br />
         <br />
-        <label>Ημερομηνία λήξης προσφοράς:</label><input type='text' id='demo2' maxlength='10' name='end_date' readonly size='10'></input>
+        <label>Ημερομηνία λήξης προσφοράς:</label><input type='text' id='demo2' maxlength='10' name='end_date' readonly size='10'
+        <?php
+        if (isset($_SESSION['end_date'])){
+            echo "value=" . $_SESSION['end_date'] ;
+        }
+        unset ($_SESSION['end_date']);
+        ?> >
         <img src="../../public/js/date_picker/images2/cal.gif" onclick="javascript:NewCssCal('demo2','yyyyMMdd','','','','','future')" style="cursor:pointer"/>   
         <span class='error'>* 
         <?php if (isset ($_SESSION['error_edate'])) {
@@ -47,7 +70,13 @@
         ?></span>
         <br />
         <br />
-        <label>Ποσοστό έκπτωσης (%):</label><input type='text' name='discount' maxlength='4' id="add_discount"></input>
+        <label>Ποσοστό έκπτωσης (%):</label><input type='text' name='discount' maxlength='4' id="add_discount"
+        <?php
+        if (isset($_SESSION['discount'])){
+            echo "value=" . $_SESSION['discount'] ;
+        }
+        unset ($_SESSION['discount']);
+        ?> >
         <span class='error'>* 
         <?php if (isset ($_SESSION['error_discount'])) {
                 echo $_SESSION['error_discount']; 
@@ -56,7 +85,13 @@
         ?></span>
         <br />
         <br />
-        <label>Τιμή μετά την έκπτωση (€):</label><input type='text' name='price' maxlength='8' id="add_price"></input>
+        <label>Τιμή μετά την έκπτωση (€):</label><input type='text' name='price' maxlength='8' id="add_price"
+        <?php
+        if (isset($_SESSION['price'])){
+            echo "value=" . $_SESSION['price'] ;
+        }
+        unset ($_SESSION['price']);
+        ?> >
         <span class='error'>* 
         <?php if (isset ($_SESSION['error_price'])) {
                 echo $_SESSION['error_price']; 
@@ -65,7 +100,7 @@
         ?></span>
         <br />
         <br />
-    <input type="submit" value="Εγγραφή" class="buttons"/>
+    <input type="submit" value="Εγγραφή" class="buttons">
 </form>
 <?php
     if (isset($_SESSION['success'])){
